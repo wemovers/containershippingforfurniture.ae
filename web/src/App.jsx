2e4eee8b397d8143b0ca805e7fc88405c14aa15d
mahaazkar321 <eemankhalid04@gmail.com>
2025-12-04 18:51:43 +0500

    comiting

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -12,12 +12,13 @@ import '../src/assets/scss/bootstrap.scss'
 import MainLayout from './components/MainLayout';
 import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
-
+import BlogPost from './pages/BlogPost';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="blog" element={<BlogPage />} />
+      <Route path="blog/:slug" element={<BlogPost />} />
       
     </Route>
   )
